c0736e334cee1ac7229f1ed89fe042399056e455
Sahashirokov <90800840+Sahashirokov@users.noreply.github.com>
2022-12-10 00:36:00 +0500

    test

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+namespace Crider2
+{
+    internal class Program
+    {
+        static int TwoMyArray( int a)
+        {
+            int[] MyArray = new int[a] ;
+            int count = 0;
+ 
+            Random rnd = new Random();
+ 
+            for (int i = 0; i < MyArray.Length; i++)
+            {
+                MyArray[i] =rnd.Next(-15,15);
+                Console.Write($"{MyArray[i]} ");
+                if (MyArray[i]>0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+        public static void Main(string[] args)
+        {
+             int a  = int.Parse(Console.ReadLine());
+ 
+            int b  = int.Parse(Console.ReadLine());
+ 
+            int count =  TwoMyArray( a);
+            Console.WriteLine();
+           Thread.Sleep(1000);
+            int count2=TwoMyArray(b);
+            Console.WriteLine();
+            Console.WriteLine($"кол-во положительных эл 1 массива = {count}, второго =  {count2}");
+            
+           
+        }
+    }
+}
